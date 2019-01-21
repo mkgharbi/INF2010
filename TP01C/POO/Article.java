d15e6b1b9dc8ca5e8c3a2b130cfe882e130576b2
Gharbi Mohamed Khairallah <lmkg97@gmail.com>
2019-01-21 17:53:13 -0500

    MKG : ajout TP1

---
+++ TP01C/POO/Article.java
@@ -0,0 +1,50 @@
+package POO;
+
+public class Article {	
+	private final String NumProduit; 
+	private String nom;
+	private double Prix_net;
+
+
+    public Article(String NumProduit, String nom){
+    	this.NumProduit = numProduit; 
+    	this.nom = nom; 
+	}
+	
+	public Article(String np, String nom, double prix){		
+		this.NumProduit = np;
+		this.nom = nom; 
+		this.Prix_net = prix; 
+	}
+	
+
+	public String getNumProduit() {
+		return NumProduit; 
+	}	
+	public String getNom() {
+		return nom; 
+	}
+	public void setNom(String nom) {
+		this.nom = nom; 
+	}
+	public double getPrixNet() {
+		return Prix_net; 
+	}
+		
+	public void setPrixNet(double prix_net) {
+		this.Prix_net = prix_net; 
+	}
+	public double getVAT(){
+		// VAT = 15% du prix_net
+		return (Prix_net/100)*15 ; 
+	}
+	public double getPrix(int count){
+		//completer
+	}
+	public String getArticleType(){
+		//completer
+	}
+	public String toString() {
+		//completer
+	}
+}
---
+++ TP01C/POO/Etudiant.java
@@ -0,0 +1,108 @@
+package POO;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Etudiant {
+	final int N = 10;
+	private String Matr;
+	private String nom;
+	private String prenom;	
+	private String email; 
+	private int section; 
+	
+    private int n_des_notes;
+	private NoteCours [] notes;
+	
+	
+	
+	 public Etudiant(String Matr,String nom, int section) {
+              this.Matr = Matr; 
+              this.nom = nom; 
+              this.section = section; 
+	    }
+        
+         
+	
+	public void AjouterNote(String sigle, String titre, int note){ 
+		
+		
+		
+		if( notes.length == n_des_notes ) {
+			NoteCours [ ] old = notes;
+			notes = new NoteCours[ n_des_notes * 2 + 1 ];
+			for(int i = 0; i < n_des_notes ; i++ )
+				notes[ i ] = old[ i ];
+		}
+		//Ajout de l'element a la position n_des_notes  du tableau et l'incrementer :  
+		notes[n_des_notes].sigle = sigle;
+		notes[n_des_notes].titre = titre;
+		notes[n_des_notes].note = note;
+			
+		n_des_notes++ ;
+		
+	}
+	
+	public double NoteMoyenne(){
+		double moyenne = 0 ;
+		for (int i =0 ; i < notes.length ; i++ ) {
+			moyenne += notes[i].note;  	// Ajouter l'element note de chaque case du tableau notes. 
+		}
+		moyenne = moyenne / (notes.length);
+		
+		return moyenne;
+	}
+
+	public String getMatr() {
+		return this.Matr; 
+	}
+
+	public void setMatr(String matr) {
+		this.Matr = matr; 
+	}
+
+    public String getEmail() {
+    	return this.email; 
+	}
+
+	public void setEmail(String email) {
+        this.email = email; 
+	}
+
+	public String getNom() {
+		return this.nom; 
+	}
+    
+	
+	public void setNom(String nom) {
+		this.nom = nom; 
+	}
+
+	public String getPrenom() {
+		return this.prenom; 
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;	}
+	
+	public int getSection() {
+		return section  ;
+	}
+	
+    @Override
+	public String toString() {
+		return "Etudiant [N=" + N + ", Matr=" + Matr + ", nom=" + nom + ", prenom=" + prenom + ", email=" + email
+				+ ", section=" + section + ", n_des_notes=" + n_des_notes + ", notes=" + Arrays.toString(notes) + "]";
+	}
+    
+    public boolean equals(Object etudiant_x) { 
+    	// On suppose qu'il existe un unique immatricule
+    	//pour chaque etudiant. La comparaison peut se faire 
+    	//seulement en comparant les chaines de Matr 
+    	//de l'etudiant_x et de l'etudiant(this)
+    	return (((Etudiant) etudiant_x).getMatr() == this.Matr) ? true : false; //Operateur Terne
+    }
+    
+   
+
+}
